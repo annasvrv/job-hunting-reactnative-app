570e7131e0a9973b148dd95d4cea0da8e6fa8321
Anna <anna.svrva@gmail.com>
2024-04-05 18:00:52 +0300

    created popular jobs component, useFetch hook and rapid Api

--- app/_layout.js
+++ app/_layout.js
@@ -1,24 +1,53 @@
 import { Stack } from "expo-router";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 
 SplashScreen.preventAutoHideAsync();
 
+// const Layout = () => {
+//   const [fontsLoaded] = useFonts({
+//     DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+//     DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+//     DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+//   });
+
+//   const onLayoutRootView = useCallback(async () => {
+//     if (fontsLoaded) {
+//       await SplashScreen.hideAsync();
+//     }
+//     [fontsLoaded];
+//     if (!fontsLoaded) return "Font problem";
+//   });
+
+//   return <Stack onLayout={onLayoutRootView} />;
+// };
+
 const Layout = () => {
-  const [fontsLoaded] = useFonts({
+  const [fontsLoaded, fontError] = useFonts({
     DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
     DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
     DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
   });
 
+  useEffect(() => {
+    async function prepare() {
+      SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  });
+
   const onLayoutRootView = useCallback(async () => {
+    console.log("Fonts loaded?", fontsLoaded);
     if (fontsLoaded) {
+      console.log("Fonts loaded");
       await SplashScreen.hideAsync();
     }
-    [fontsLoaded];
-    if (!fontsLoaded) return null;
-  });
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return undefined;
+  }
 
   return <Stack onLayout={onLayoutRootView} />;
 };
---
+++ hook/useFetch.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "4dfd002583msh0927f008e5e1c3ep114f89jsn4ca05ccfd8ed",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: {
+      query: { ...query },
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
