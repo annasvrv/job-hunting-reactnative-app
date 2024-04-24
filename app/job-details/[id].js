324a88f7341c9333f2e7737088e889a4f4a260c1
Anna <anna.svrva@gmail.com>
2024-04-24 14:50:39 +0300

    created dinamic components for job details About, Footer, Specifics

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -20,7 +20,7 @@ import {
 import { COLORS, SIZES, icons } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
-const tabs = ["About", "Qualifications", "Responsibilites"];
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 const JobDetails = () => {
   const params = useSearchParams();
@@ -34,6 +34,30 @@ const JobDetails = () => {
   const [activeTab, setActiveTab] = useState(tabs[0]);
 
   const onRefresh = () => {};
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return (
+          <Specifics
+            title="Qualifications"
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+      case "About":
+        return (
+          <JobAbout info={data[0].job_description ?? "No data provided"} />
+        );
+      case "Responsibilities":
+        return;
+        <Specifics
+          title="Responsibilities"
+          points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+        />;
+      default:
+        break;
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -54,43 +78,51 @@ const JobDetails = () => {
           ),
           headerTitle: "",
         }}
-      >
-        <>
-          <ScrollView
-            showsVerticalScrollIndicator={false}
-            refreshControl={
-              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-            }
-          >
-            {isLoading ? (
-              <ActivityIndicator size="large" color={COLORS.primary} />
-            ) : error ? (
-              <Text>Error</Text>
-            ) : data.length === 0 ? (
-              <Text>No Data</Text>
-            ) : (
-              <View
-                style={{
-                  padding: SIZES.medium,
-                  paddingBottom: 100,
-                }}
-              >
-                <Company
-                  companyLogo={data[0].employer_logo}
-                  jobTitle={data[0].job_title}
-                  companyNam={data[0].employer_name}
-                  location={data[0].job_country}
-                />
-                <JobTabs
-                  tabs={tabs}
-                  activeTab={activeTab}
-                  setActiveTab={setActiveTab}
-                />
-              </View>
-            )}
-          </ScrollView>
-        </>
-      </Stack.Screen>
+      />
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          ) : error ? (
+            <Text>Error</Text>
+          ) : data.length === 0 ? (
+            <Text>No Data</Text>
+          ) : (
+            <View
+              style={{
+                padding: SIZES.medium,
+                paddingBottom: 100,
+              }}
+            >
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyNam={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+
+              {displayTabContent()}
+            </View>
+          )}
+        </ScrollView>
+
+        <JobFooter
+          url={
+            data[0]?.job_google_link ??
+            "https://careers.google.com/jobs/results"
+          }
+        />
+      </>
     </SafeAreaView>
   );
 };
